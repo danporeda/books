934117db67ce9a0c8e4431ecf8b00ee4ec900139
Dan <danporeda@yahoo.com>
2024-04-16 00:29:11 -0400

    imported createContext into context/books.js. export of Provider module and BooksContent

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,16 @@
+import { useContext } from 'react';
+import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
 function BookList({ onEdit, books, onDelete }) {
+  const value = useContext(BooksContext);
+
   const renderedBooks = books.map((book) => {
     return <BookShow onEdit={onEdit} key={book.id} book={book} onDelete={onDelete} />;
   });
 
-  return <div className="book-list">{renderedBooks}</div>;
+  return <div className="book-list">{value}
+  {renderedBooks}</div>;
 }
+
 export default BookList;
