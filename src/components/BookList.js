7f374df426b549b241de4d4ef740deaa7ad6c30e
Dan <danporeda@yahoo.com>
2024-04-16 23:29:41 -0400

    refactored with createContext in books.js, migrated application state and functions there (books, createBooks, editBookById, deleteBookById, fetchBooks)

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,14 +3,18 @@ import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
 function BookList({ onEdit, books, onDelete }) {
-  const value = useContext(BooksContext);
+  
 
   const renderedBooks = books.map((book) => {
     return <BookShow onEdit={onEdit} key={book.id} book={book} onDelete={onDelete} />;
   });
 
-  return <div className="book-list">{value}
-  {renderedBooks}</div>;
+  return (
+    <div className="book-list">
+      
+      {renderedBooks}
+    </div>
+  );
 }
 
 export default BookList;
